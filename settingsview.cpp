6a0623c77651f7f3b3389b239a3f5baab9b05ca4
mberkayk <berkaykuman@live.com>
2021-02-18 10:52:51 +0300

    Added savable settings and deafults for entry descriptions

--- settingsview.cpp
+++ settingsview.cpp
@@ -1,11 +1,69 @@
 #include "settingsview.h"
 
-SettingsView::SettingsView() : QWidget (){
+
+EditableSettingLabel::EditableSettingLabel(QString _setting, QString label) : QLabel(label) {
+	setting = _setting;
+	grabGesture(Qt::TapAndHoldGesture);
+}
+
+bool EditableSettingLabel::event(QEvent *event){
+	if(event->type() == QEvent::Gesture){
+		QGestureEvent *gEvent = static_cast<QGestureEvent*>(event);
+		if(QGesture *hold = gEvent->gesture(Qt::TapAndHoldGesture)){
+			if(hold->state() == Qt::GestureFinished){
+				setText(QInputDialog::getText(this, "Edit Setting", "Enter A New Value:", QLineEdit::Normal, text()));
+				emit setttingEditedSignal(this);
+			}
+		}
+	}
+
+	return QWidget::event(event);
+}
+
+QString EditableSettingLabel::getSetting(){return setting;}
+
+
+SettingsView::SettingsView() : QWidget () , settings("mberkayk", "Budget-App"){
 
 	layout = new QVBoxLayout;
-        setLayout(layout);
+	setLayout(layout);
 
 	title = new QLabel("Settings View");
 	layout->addWidget(title);
 
+	if(settings.contains("default daily description") == false)
+		settings.setValue("default daily description", "Food");
+
+	if(settings.contains("default weekly description") == false)
+		settings.setValue("default weekly description", "Groceries");
+
+	if(settings.contains("default monthly description") == false)
+		settings.setValue("default monthly description", "Monthly Bill");
+
+	QFormLayout *formLayout = new QFormLayout;
+	layout->addLayout(formLayout);
+
+	dailyDefaultEditableSetting = new EditableSettingLabel("default daily description",
+														   settings.value("default daily description").toString());
+	formLayout->addRow(new QLabel("Default Daily Description: "), dailyDefaultEditableSetting);
+	QObject::connect(dailyDefaultEditableSetting, &EditableSettingLabel::setttingEditedSignal,
+					 this, &SettingsView::settingEditedSlot);
+
+
+	weeklyDefaultEditableSetting = new EditableSettingLabel("default weekly description",
+															settings.value("default weekly description").toString());
+	formLayout->addRow(new QLabel("Default Weekly Description: "), weeklyDefaultEditableSetting);
+	QObject::connect(weeklyDefaultEditableSetting, &EditableSettingLabel::setttingEditedSignal,
+					 this, &SettingsView::settingEditedSlot);
+
+	monthlyDefaultEditableSetting = new EditableSettingLabel("default monthly description",
+														   settings.value("default monthly description").toString());
+	formLayout->addRow(new QLabel("Default monthly Description: "), monthlyDefaultEditableSetting);
+	QObject::connect(monthlyDefaultEditableSetting, &EditableSettingLabel::setttingEditedSignal,
+					 this, &SettingsView::settingEditedSlot);
+
+}
+
+void SettingsView::settingEditedSlot(EditableSettingLabel* label){
+	settings.setValue(label->getSetting(), label->text());
 }
--- settingsview.h
+++ settingsview.h
@@ -4,6 +4,33 @@
 #include <QWidget>
 #include <QVBoxLayout>
 #include <QLabel>
+#include <QSettings>
+#include <QFormLayout>
+#include <QTapAndHoldGesture>
+#include <QInputDialog>
+
+
+
+class EditableSettingLabel : public QLabel {
+	Q_OBJECT
+
+public:
+	EditableSettingLabel(QString, QString);
+
+	QString getSetting();
+
+private:
+
+	QString setting;
+
+	bool event(QEvent*) override;
+
+signals:
+	void setttingEditedSignal(EditableSettingLabel*);
+
+};
+
+
 
 class SettingsView : public QWidget {
 
@@ -13,10 +40,18 @@ private:
 	QVBoxLayout *layout;
 	QLabel *title;
 
+	QSettings settings;
+	EditableSettingLabel *dailyDefaultEditableSetting;
+	EditableSettingLabel *weeklyDefaultEditableSetting;
+	EditableSettingLabel *monthlyDefaultEditableSetting;
+
 public:
 	SettingsView();
 
 	QWidget *getWidget();
+
+private slots:
+	void settingEditedSlot(EditableSettingLabel*);
 };
 
 #endif // SETTINGSVIEW_H
